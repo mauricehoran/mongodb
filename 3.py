c8442e4bdcd918ffc66ac81f3de976352edaf5b0
mauricehoran <horan.maurice@gmail.com>
2021-05-05 11:09:20 +0000

    code

--- 3.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-myclient = MongoClient('172.17.0.2',27017)
-mydb = myclient["mauricedb"]
-mycoll = mydb["mhcoll"]
-
-for result in mycoll.find():
-        print(result)
